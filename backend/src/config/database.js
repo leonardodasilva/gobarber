a9a3d06d5ba6e095e1130c74294102c734aa5853
Leonardo da Silva <dasilva.leonardo@gmail.com>
2019-12-03 18:00:18 -0300

    Criando usuarios

---
+++ backend/src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: '10.0.75.1',
+  username: 'postgres',
+  password: 'postgres',
+  database: 'gobarber',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
