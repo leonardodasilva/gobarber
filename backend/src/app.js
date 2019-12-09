f4206d904c06228a63bf20c1209968b5e5c086c7
Leonardo da Silva <dasilva.leonardo@gmail.com>
2019-12-09 10:57:31 -0300

    Aplicando Segurança

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,10 @@ import express from 'express';
 import 'express-async-errors';
 import Youch from 'youch';
 import cors from 'cors';
+import helmet from 'helmet';
+import redis from 'redis';
+import RateLimit from 'express-rate-limit';
+import RateLimitRedis from 'rate-limit-redis';
 import path from 'path';
 import * as Sentry from '@sentry/node';
 import routes from './routes';
@@ -23,12 +27,32 @@ class App {
 
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
-    this.server.use(cors());
+    this.server.use(helmet());
+    this.server.use(
+      cors({
+        origin: process.env.FRONT_URL,
+      })
+    );
     this.server.use(express.json());
     this.server.use(
       '/files',
       express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
     );
+
+    if (process.env.NODE_ENV !== 'development') {
+      this.server.use(
+        new RateLimit({
+          store: new RateLimitRedis({
+            client: redis.createClient({
+              host: process.env.REDIS_HOST,
+              port: process.env.REDIS_PORT,
+            }),
+          }),
+          windowMs: 1000 * 60 * 15,
+          max: 100,
+        })
+      );
+    }
   }
 
   routes() {
